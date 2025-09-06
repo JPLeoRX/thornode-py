8941d5eaf488e9237d852ad2b769d9c17560315d
jpleorx <leo.ertuna@gmail.com>
2025-09-06 21:30:53 +0300

    Moving more tests

--- thornode_py/main_test.py
+++
@@ -1,8 +0,0 @@
-from thornode_api import THORNodeAPI
-
-api = THORNodeAPI()
-
-
-# print("Liquidity Providers:")
-# print(len(api.liquidity_providers("BTC.BTC")))
-# print(api.liquidity_provider("BTC.BTC", "bc1q00nrswtpp3zddgc0uvppuszhnr8k8zfcdps9gn"))
